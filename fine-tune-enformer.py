55a9a042753c1ed6b0fe6ff38cf18fe1ae294ada
Clément Duffau <duffauclem@gmail.com>
2025-03-19 18:40:03 +0100

    trainer enformer lightning

--- fine-tune-enformer.py
+++ fine-tune-enformer.py
@@ -1,226 +1,220 @@
 import os
-
-import cv2
+import wandb
 import numpy as np
 import pytorch_lightning as pl
-import timm
 import torch
 import torch.nn as nn
 import torchmetrics
+from torch.utils.data import DataLoader
 from sklearn.metrics import precision_recall_curve
 from grelu.lightning.metrics import MSE, BestF1, PearsonCorrCoef
 from torchmetrics import AUROC, Accuracy, AveragePrecision, MetricCollection
 from torch import Tensor, nn, optim
 from typing import Callable, List, Optional, Tuple, Union
 import logging
+from tangermeme.utils import one_hot_encode
 import pandas as pd
+from enformer_pytorch.finetune import HeadAdapterWrapper
 import grelu.lightning
+from grelu.data.dataset import LabeledSeqDataset
+from pytorch_lightning.loggers import WandbLogger
 import pyBigWig
+import scipy.stats
 import pytorch_lightning as pl
 from datasets import Dataset, DatasetDict
 from enformer_pytorch.data import str_to_seq_indices
+from enformer_pytorch import Enformer
 from grelu.io.genome import get_genome
 from intervals import (
     get_defined_intervals,
     expression_bw,
-    create_interval_n
+    create_interval_n,
+    expression_val_histone,
+    get_mapability,
+    process_sequences,
+    get_value_bw
 )
+from model import load_enformer_model
+from pytorch_lightning import Trainer
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-"""
-class LightningModel(pl.LightningModule):
-    def __init__(self, args):
-        super().__init__()
-        self.args = args
-        self.lr = args.lr
-        self.weight_decay = args.weight_decay
-        self.eval_every_n_steps = args.eval_every_n_steps
-        self.model = load_enformer_model(num_tracks=len(args.signal_bw_paths))
-        self.loss = nn.MSELoss()
-        self.activation = nn.Identity()
-        self.val_losses = []
-    
-    def forward(self, x):
-        x = self.model(x)
-        return self.activation(x)
-    
-    def training_step(self, batch, batch_idx):
-
-        x, y = batch["input_ids"], batch["labels"]
-        logits = self(x)
-        loss = self.loss(logits, y)
-        for name, param in self.named_parameters():
-            if param.grad is not None:
-                self.logger.experiment.add_histogram(f"gradients/{name}", param.grad, self.global_step)
-        
-        self.log("train_loss", loss, prog_bar=True, logger=True)
-        return loss
-    
-    def validation_step(self, batch, batch_idx):
-        if (self.global_step + 1) % self.eval_every_n_steps == 0:
-            x, y = batch["input_ids"], batch["labels"]
-            logits = self(x)
-            loss = self.loss(logits, y)
-            self.log("val_loss", loss, prog_bar=True, logger=True)
-            self.val_losses.append(loss)
-            return loss
-        return None
-    
-    def on_validation_epoch_end(self):
-        if self.val_losses:
-            mean_loss = torch.stack(self.val_losses).mean()
-            self.log("mean_val_loss", mean_loss, prog_bar=True, logger=True)
-            self.val_losses.clear()
-    
-    def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-    
-    def initialize_metrics(self):
-        
-        metrics = MetricCollection(
-            {
-                "mse": MSE(num_outputs=self.model.head.n_tasks, average=False),
-                "pearson": PearsonCorrCoef(
-                    num_outputs=self.model.head.n_tasks, average=False
-                ),
-            }
-        )
-        return metrics
-
-    def update_metrics(self, metrics: dict, y_hat: Tensor, y: Tensor) -> None:
-        
-        metrics.update(y_hat, y)
-    
-    def format_input(self, x: Union[Tuple[Tensor, Tensor], Tensor]) -> Tensor:
-
-        # if x is a tuple of sequence, label, return the sequence
-        if isinstance(x, Tensor):
-            if x.ndim == 3:
-                return x
-            else:
-                return x.unsqueeze(0)
-        elif isinstance(x, Tuple):
-            return x[0]
-        else:
-            raise Exception("Cannot perform forward pass on the given input format.")
-
-"""
-
-
-
-model_params = {
-    'model_type': 'EnformerPretrainedModel',
-    'n_tasks': 1,  
-    'crop_len': 0,
-    'n_transformers': 1,
-}
+wandb_logger = WandbLogger(project="genome-prediction")
 
 train_params = {
-    'task':'regression',
-    'loss': 'poisson',
     'lr': 1e-4,
-    'logger': 'wandb',
     'batch_size': 2,
-    'num_workers': 1,
+    'num_workers': 31,
     'devices': 0,
     'save_dir': "./experiment",
-    'optimizer': 'adam',
     'max_epochs': 5,
     'checkpoint': True,
-    'val_check_interval': 500,
+    'val_check_interval' : 100
 }
 
+class DataModule(pl.LightningDataModule):
+    def __init__(self, args, train_params):
+        super().__init__()
+        self.save_hyperparameters()
+        self.args = args
+        self.train_params = train_params
+        self.train_ds = None
+        self.val_ds = None
+        self.test_ds = None
 
-def prepare_batch(batch, genome, signal_bw_paths,mappability_bw_path, bin_size=128, ignore_low_mappa=True, ignore_low_mappa_threshold=0.9):
-    signal_bws = [pyBigWig.open(str(bw_path)) for bw_path in signal_bw_paths]
-    mappa_bw = pyBigWig.open(mappability_bw_path) if mappability_bw_path else None
-    seqs, targets, ignore_masks = [], [], []
-    
-    for i in range(len(batch["start"])):
-        chrom, start, end = batch["chrom"][i], batch["start"][i], batch["end"][i]
-        seq = genome.get_seq(chrom, start + 1, end).seq.upper()
-        seqs.append(seq)
-        
-        target = []
-        for signal_bw in signal_bws:
-            signal_nt = signal_bw.values(chrom, start, end, numpy=True)
-            signal_b = expression_bw(np.nan_to_num(signal_nt), bin_size, statistic="mean")
-            target.append(signal_b)
-        target = np.stack(target, axis=1)
-        targets.append(target)
-        
-        ignore_mask = np.isnan(target) | (target == 0)
-        
-        if ignore_low_mappa and mappa_bw:
-            mappa_nt = mappa_bw.values(chrom, start, end, numpy=True)
-            mappa_b = expression_bw(np.nan_to_num(mappa_nt), bin_size, statistic="mean")
-            low_mappa_mask = (mappa_b <= ignore_low_mappa_threshold)
-            ignore_mask = ignore_mask | low_mappa_mask
-        
-        ignore_masks.append(ignore_mask)
-    
-    if mappa_bw:
-        mappa_bw.close()
-    
-    return {"input_ids": str_to_seq_indices(seqs), "labels": targets, "ignore_mask": ignore_masks}
+    def setup(self, stage = None):
+        if stage == "fit":
+            genome = get_genome(self.args.assembly)
+            intervals = get_defined_intervals(genome, self.args.train_chroms + self.args.eval_chroms + self.args.test_chroms, min_length=self.args.seq_length)
+            windows = create_interval_n(intervals, self.args.seq_length, self.args.bin_size, self.args.target_length_b * self.args.bin_size)
+
+            df_train = pd.concat([windows[chrom] for chrom in self.args.train_chroms], axis=0)
+            df_val = pd.concat([windows[chrom] for chrom in self.args.eval_chroms], axis=0)
+
+            train_data, val_data, _ = get_value_bw(self.args.signal_bw_paths, self.args.mappability_bw_path, df_train, df_val, pd.DataFrame,stage, self.args.bin_size)
+            
+            train_ds = Dataset.from_pandas(train_data)
+            train_ds.set_format("torch")
+            val_ds = Dataset.from_pandas(val_data)
+            val_ds.set_format("torch")
+
+            self.train_ds = process_sequences(train_ds, genome)
+            self.val_ds = process_sequences(val_ds, genome)
+
+        if stage == "test" :
+            genome = get_genome(self.args.assembly)
+            df_test = pd.concat([windows[chrom] for chrom in self.args.test_chroms], axis=0)
+
+            _, _, test_data = get_value_bw(
+                self.args.signal_bw_paths,
+                self.args.mappability_bw_path,
+                pd.DataFrame(), 
+                pd.DataFrame(),
+                df_test,
+                self.args.bin_size,
+            )
+
+            self.test_ds = process_sequences(Dataset.from_pandas(test_data), genome)
 
 
-def load_data(args):
-    genome = get_genome(args.assembly)
-    intervals = get_defined_intervals(genome, args.train_chroms + args.eval_chroms, min_length=args.seq_length)
-    windows = create_interval_n(intervals, args.seq_length, args.target_length_b * args.bin_size)
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.train_params['batch_size'], num_workers=self.train_params['num_workers'], shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=self.train_params['batch_size'], num_workers=self.train_params['num_workers'], shuffle = False)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, batch_size=self.train_params['batch_size'], num_workers=self.train_params['num_workers'], shuffle = False)
+
+
+
+
+
+class Enformerlightning(pl.LightningModule):
+    def __init__(self, train_params):
+        super().__init__()
+        self.save_hyperparameters()
+        self.backbone = load_enformer_model()
+        self.model = HeadAdapterWrapper(enformer = self.backbone,num_tracks=1)
+        self.loss_fn = torch.nn.functional.poisson_nll_loss 
+        self.train_mse = torchmetrics.MeanSquaredError()
+        self.val_mse = torchmetrics.MeanSquaredError()
+        #self.train_pearson = torchmetrics.PearsonCorrCoef()
+        #self.val_pearson = torchmetrics.PearsonCorrCoef()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y , z = batch["input_oh"], batch["labels"], batch["map"]
+        x = x.permute(0, 2, 1).to(dtype=torch.float16)
+        preds = self(x)
+        mask_map = (z != 0)            
+        mask = (y !=0)  
+        ignore_mask = mask_map | mask 
+        f_preds = preds[ignore_mask] 
+        f_labels = y[ignore_mask]
+        f_preds = f_preds.squeeze(-1)
+        loss = self.loss_fn(f_preds.squeeze(), f_labels.squeeze(), reduction = 'mean')
+        self.log("train_loss", loss, on_epoch=True, prog_bar=True)
+        #self.log("train_mse", self.train_mse(f_preds, f_labels), on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y, z = batch["input_oh"], batch["labels"], batch["map"]
+        x = x.permute(0, 2, 1).to(dtype=torch.float16)
+        preds = self(x)
+        mask_map = (z != 0)            
+        mask = (y !=0)  
+        ignore_mask = mask_map | mask  
+        f_preds = preds[ignore_mask].squeeze(-1)
+        f_labels = y[ignore_mask]
+        
+        loss = self.loss_fn(f_preds.squeeze(), f_labels.squeeze(), reduction = 'mean')
+
+        if f_preds.numel() > 1 and f_labels.numel() > 1:
+            pearson_score = scipy.stats.pearsonr(f_preds.cpu().numpy().flatten(), 
+                                             f_labels.cpu().numpy().flatten())[0]
+        else:
+            pearson_score = float('nan')
+
+        self.log("val_loss", loss, on_epoch=True, on_step=True, prog_bar=True, logger=True)
+        self.log("val_mse", self.val_mse(f_preds, f_labels), on_epoch=True,on_step=True, prog_bar=True, logger= True)
+        self.log("val_pearson", pearson_score, on_epoch=True, prog_bar=True, logger=True)
+
+        return {"val_loss": loss, "val_pearson": pearson_score}
     
-    
-    ds = DatasetDict({
-        "train": Dataset.from_pandas(windows[windows.chrom.isin(args.train_chroms)]),
-        "eval": Dataset.from_pandas(windows[windows.chrom.isin(args.eval_chroms)]),
-        "test" : Dataset.from_pandas(windows[windows.chrom.isin(args.test_chroms)])
-    })
-    
-    ds = ds.map(lambda batch: prepare_batch(batch, genome, args.signal_bw_paths, args.bin_size), batched=True)
-    return ds
+    def on_validation_epoch_end(self):
+        avg_loss = self.trainer.callback_metrics.get("val_loss", torch.tensor(float('nan'))).item()
+        avg_pearson = self.trainer.callback_metrics.get("val_pearson", torch.tensor(float('nan'))).item()
+
+        print(f" Validation Epoch End - Loss: {avg_loss:.4f}, Pearson: {avg_pearson:.4f}")
+
+        self.log("avg_val_loss", avg_loss, prog_bar=True, logger=True)
+        self.log("avg_val_pearson", avg_pearson, prog_bar=True, logger=True)
+
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.hparams.train_params['lr'])
+
+
+class Args:
+        assembly = "hs1"
+        signal_bw_paths = ["SRR18582971.signal.bl_s8_exz.bw"]
+        mappability_bw_path = ["k36.Umap.MultiTrackMappability.bw"]
+        seq_length, bin_size, target_length_b = 196608, 128, 896
+        train_chroms, eval_chroms, test_chroms = [f"chr{i}" for i in range(1, 2)], ["chr14"], ["chr15"]
 
 def main():
-    class Args:
-        assembly = "hg38"
-        signal_bw_paths = "SRR18582971.signal.bl_s8_exz.bw"
-        mappability_bw_path = "k36.Umap.MultiTrackMappability.bw"
-        seq_length, bin_size, target_length_b = 196608, 128, 896
-        train_chroms, eval_chroms, test_chroms = [f"chr{i}" for i in range(1, 14)], ["chr14"], ["chr15"]
-        lr = 1e-4
-        weight_decay = 1e-5
-        eval_every_n_steps = 500
-    
+
     args = Args()
-    dataset = load_data(args)
-    print(dataset)
-    model_params['n_tasks'] = len(args.signal_bw_paths) 
-
-    experiment='experiment'
-    if not os.path.exists(experiment):
-        os.makedirs(experiment)
-
-    model = grelu.lightning.LightningModel(model_params=model_params, train_params=train_params)
-    trainer = model.train_on_dataset(
-        train_dataset=dataset["train"],
-        val_dataset=dataset["eval"],
+    data_module  = DataModule(args, train_params=train_params)
+    data_module.setup(stage = "fit")
+    model = Enformerlightning(train_params=train_params)
+    
+    trainer = pl.Trainer(
+        max_epochs=train_params['max_epochs'],
+        val_check_interval=train_params['val_check_interval'],
+        logger=wandb_logger,
+        log_every_n_steps=100,
+        accelerator="gpu",
     )
+
+    trainer.fit(model, data_module)
+
+
+    wandb.log({"mse": trainer.logged_metrics['mse'],
+               "pearson": trainer.logged_metrics['pearson'],
+               "loss": trainer.logged_metrics['loss']})
+    
+
     best_checkpoint = trainer.checkpoint_callback.best_model_path
     print(best_checkpoint)
 
-    model = grelu.lightning.LightningModel.load_from_checkpoint(best_checkpoint)
-
-    test_metrics = model.test_on_dataset(
-    test_ds = dataset["test"],
-    batch_size=4,
-    devices=0,
-    num_workers=1
-    )
-
-    test_metrics
+    
 
 if __name__ == "__main__":
     main()
-
+    wandb.finish()
    
     
